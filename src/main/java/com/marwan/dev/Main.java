b857b5f068954ca4ebc9fb636ac37fb673836c0e
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-11 04:05:34 +0300

    add the raw idea

---
+++ src/main/java/com/marwan/dev/Main.java
@@ -0,0 +1,13 @@
+package com.marwan.dev;
+
+import com.marwan.dev.employee.model.Employee;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Employee employee1 = new Employee(90, "Mohamed", "Ayman");
+
+    System.out.print(employee1);
+  }
+}
---
+++ src/main/java/com/marwan/dev/employee/model/Employee.java
@@ -0,0 +1,5 @@
+package com.marwan.dev.employee.model;
+
+public record Employee(int id, String fName, String lName) {
+
+}
